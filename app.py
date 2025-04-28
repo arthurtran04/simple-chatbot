f52631cad0a283296ee419f813f6d1e5a6d7cc46
Minh Phat <minhphat.workmail@gmail.com>
2025-04-28 16:29:29 +0700

    Add sources

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS
 import json
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+# from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 app = Flask(__name__)
 CORS(app)
 
-model_name = "facebook/blenderbot-400M-distill"
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model_name = "facebook/blenderbot-400M-distill"
+# model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+model_name = "google/flan-t5-base"
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name, device_map="auto")
 conversation_history = []
 
 @app.route('/chatbot', methods=['POST'])
@@ -22,9 +26,11 @@ def handle_prompt():
 
     # Tokenize the input text and history
     inputs = tokenizer.encode_plus(history, input_text, return_tensors="pt")
+    # input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 
     # Generate the response from the model
     outputs = model.generate(**inputs, max_length= 60)  # max_length will cause the model to crash at some point as history grows
+    # outputs = model.generate(input_ids)
 
     # Decode the response
     response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
@@ -35,5 +41,9 @@ def handle_prompt():
 
     return response
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)
