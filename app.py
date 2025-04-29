65e04a6404e4910af36c2d3ef9510da4f524d815
Minh Phat <minhphat.workmail@gmail.com>
2025-04-29 15:40:42 +0700

    Update app.y and front-end

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
+'''
+This is the main file for the chatbot application.
+It is a simple Flask application that allows users to chat with a chatbot.
+The chatbot is based on the Blenderbot model from Facebook.
+'''
+# Importing the necessary libraries
+import json
 from flask import Flask, request, render_template
 from flask_cors import CORS
-import json
-# from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
+# Initializing the Flask application
 app = Flask(__name__)
 CORS(app)
 
-# model_name = "facebook/blenderbot-400M-distill"
-# model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-model_name = "google/flan-t5-base"
-tokenizer = T5Tokenizer.from_pretrained(model_name)
-model = T5ForConditionalGeneration.from_pretrained(model_name, device_map="auto")
+# Loading the Blenderbot model and tokenizer
+MODEL_NAME = "facebook/blenderbot-400M-distill"
+model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+# Initializing the conversation history
 conversation_history = []
 
+# Defining the route for the chatbot
 @app.route('/chatbot', methods=['POST'])
 def handle_prompt():
+    '''
+    This function handles the prompt from the user.
+    It gets the prompt from the request and creates a conversation history string.
+    It then tokenizes the input text and history and generates the response from the model.
+    '''
+    # Getting the prompt from the request
     data = request.get_data(as_text=True)
     data = json.loads(data)
     input_text = data['prompt']
@@ -26,11 +39,10 @@ def handle_prompt():
 
     # Tokenize the input text and history
     inputs = tokenizer.encode_plus(history, input_text, return_tensors="pt")
-    # input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 
     # Generate the response from the model
-    outputs = model.generate(**inputs, max_length= 60)  # max_length will cause the model to crash at some point as history grows
-    # outputs = model.generate(input_ids)
+    outputs = model.generate(**inputs, max_length=60)
+    # max_length will cause the model to crash at some point as history grows
 
     # Decode the response
     response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
@@ -43,7 +55,12 @@ def handle_prompt():
 
 @app.route('/')
 def home():
+    '''
+    This function renders the home page.
+    '''
+    # Rendering the home page
     return render_template('index.html')
 
+# Running the application
 if __name__ == '__main__':
     app.run(debug=False)
